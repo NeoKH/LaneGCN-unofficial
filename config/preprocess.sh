f2f86fd72d1b173cb3dc3e0e49e0464d9d3b1eb3
neokh <konghao078@gmail.com>
2022-08-27 11:11:43 +0800

    modify shell script

--- config/preprocess.sh
+++ config/preprocess.sh
@@ -1,8 +1,16 @@
+cd /root/LaneGCN-unofficial
+python data.py --start_iter 0 --workers 8 --batch_size 64 --data_type val
+
+if [ $? -eq 0 ];then
+    rm -r /hy-tmp/argoverse1/val
+    zip -r /hy-tmp/argoverse1/preprocess/val.zip /hy-tmp/argoverse1/preprocess/val
+    gpushare-cli baidu up /hy-tmp/argoverse1/preprocess/val.zip  /孔昊私人/数据集/argoverse1/
+    gpushare-cli ali up /hy-tmp/argoverse1/preprocess/val.zip  /轨迹预测数据集/argoverse1/
+    rm /hy-tmp/argoverse1/preprocess/val.zip
+fi
 
 tar -zxvf /hy-public/Argoverse/forecasting_train_v1.1.tar.gz -C /hy-tmp/argoverse1
-
 cd /root/LaneGCN-unofficial
-
 python data.py --start_iter 0 --workers 8 --batch_size 64 --data_type train
 
 if [ $? -eq 0 ];then
@@ -11,15 +19,7 @@ if [ $? -eq 0 ];then
     gpushare-cli baidu up /hy-tmp/argoverse1/preprocess/train.zip  /孔昊私人/数据集/argoverse1/
     gpushare-cli ali up /hy-tmp/argoverse1/preprocess/train.zip  /轨迹预测数据集/argoverse1/
     oss cp /hy-tmp/argoverse1/preprocess/train.zip oss:///datasets
-fi
-
-python data.py --start_iter 0 --workers 8 --batch_size 64 --data_type val
-
-if [ $? -eq 0 ];then
-    rm -r /hy-tmp/argoverse1/val
-    zip -r /hy-tmp/argoverse1/preprocess/train.zip /hy-tmp/argoverse1/preprocess/val
-    gpushare-cli baidu up /hy-tmp/argoverse1/preprocess/train.zip  /孔昊私人/数据集/argoverse1/
-    gpushare-cli ali up /hy-tmp/argoverse1/preprocess/train.zip  /轨迹预测数据集/argoverse1/
+    rm /hy-tmp/argoverse1/preprocess/train.zip
 fi
 
 shutdown
